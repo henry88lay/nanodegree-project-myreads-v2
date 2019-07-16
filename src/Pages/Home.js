fddacf6ca8b9231967d23961379302e6aa4e7b08
Henry Lay <henry88lay@gmail.com>
2019-07-16 23:12:56 +1000

    created Components
    Created Components for the following:
    Home.js
    Search.js
    FloatActionButton.js
    and installed npm i react-router-dom

---
+++ src/Pages/Home.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Shelf from '../components/Shelf';
+import FloatActionButton from '../components/FloatActionButton';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className='list-books'>
+        <div className='list-books-title'>
+          <h1>MyReads</h1>
+        </div>
+        <div className='list-books-content'>
+          <Shelf />
+        </div>
+        <FloatActionButton />
+      </div>
+    );
+  }
+}
