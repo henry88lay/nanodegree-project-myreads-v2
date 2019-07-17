668a30a893d949872cad5df0b947af1a804f5f9d
Henry Lay <henry88lay@gmail.com>
2019-07-17 21:39:56 +1000

    Cleaned up app.js
    set up the browser router on index.js
    set up the pathing of the website

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 // import * as BooksAPI from './BooksAPI'
 import './App.css';
-import Shelf from './components/Shelf';
-import FloatActionButton from './components/FloatActionButton';
+import Home from './Pages/Home';
+import Search from './Pages/Search';
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  };
-
   render() {
     return (
       <div className='app'>
-        {this.state.showSearchPage ? (
-          
-        ) : (
-          
-        )}
+        <Switch>
+          <Route exact path={'/'} component={Home} />
+          <Route exact path={'/search'} component={Search} />
+        </Switch>
       </div>
     );
   }
