173fc9a5b1552de6d8cf320b225853849cd01db8
Henry Lay <henry88lay@gmail.com>
2019-07-19 23:43:13 +1000

    setting up the routers towards the components for FAB and Search

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,16 +1,30 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {search} from '../BooksAPI'
 
 export default class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ""
+    }
+  }
+
+  handleChange = async e => {
+    try {
+      const query = e.target.value
+      this.setState({query})
+        const results = await search(query)
+        console.log(results)
+    }
+  }
   render() {
     return (
       <div className='search-books'>
         <div className='search-books-bar'>
-          <button
-            className='close-search'
-            onClick={() => this.setState({showSearchPage: false})}
-          >
+          <Link className='close-search' to={'/'}>
             Close
-          </button>
+          </Link>
           <div className='search-books-input-wrapper'>
             {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -20,7 +34,7 @@ export default class Search extends Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-            <input type='text' placeholder='Search by title or author' />
+            <input type='text' placeholder='Search by title or author' onChange={this.handleChange}/>
           </div>
         </div>
         <div className='search-books-results'>
