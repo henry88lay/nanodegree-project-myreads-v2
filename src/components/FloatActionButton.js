31460d0131179bf30ff067784095f1b9d6bd65b4
Henry Lay <henry88lay@gmail.com>
2019-07-27 00:04:13 +1000

    Refactor: FloatActionButton
    changed from class component to functional component.

--- src/components/FloatActionButton.js
+++ src/components/FloatActionButton.js
@@ -1,14 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 
-export default class FloatActionButton extends Component {
-  render() {
-    return (
-      <div className='open-search'>
-        <Link to={'/search'}>
-          <button>Add a book</button>
-        </Link>
-      </div>
-    );
-  }
+export default function FloatActionButton() {
+  return (
+    <div className='open-search'>
+      <Link to={'/search'}>
+        <button>Add a book</button>
+      </Link>
+    </div>
+  );
 }
