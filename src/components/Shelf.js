9f5f2693964e069fc4091d0f9ae43892c527e255
Henry Lay <henry88lay@gmail.com>
2019-07-27 00:01:53 +1000

    refactored Shelf.JS
    changed from class to functional component.

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,21 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Book from './Book';
 
-export default class Shelf extends Component {
-  render() {
-    const {books, moveBook, title} = this.props;
-    return (
-      <div className='bookshelf'>
-        <h2 className='bookshelf-title'>{title}</h2>
-        <div className='bookshelf-books'>
-          <ol className='books-grid'>
-            {books &&
-              books.map(book => (
-                <Book key={book.id} {...book} moveBook={moveBook} />
-              ))}
-          </ol>
-        </div>
+export default function Shelf(props) {
+  const {books, moveBook, title} = props;
+  return (
+    <div className='bookshelf'>
+      <h2 className='bookshelf-title'>{title}</h2>
+      <div className='bookshelf-books'>
+        <ol className='books-grid'>
+          {books &&
+            books.map(book => (
+              <Book key={book.id} {...book} moveBook={moveBook} />
+            ))}
+        </ol>
       </div>
-    );
-  }
+    </div>
+  );
 }
